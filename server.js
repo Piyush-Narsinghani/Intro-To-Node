c3f457c8f973a28c409088ddb00b5b10d279ad52
Piyush Narsinghani <piyushnarsinghani13@gmail.com>
2020-11-27 16:13:44 +0530

    solve error 4

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 var http = require('http');
+var fs = require('fs');
 var url = require('url');
+const PORT = process.env.PORT || 5000
+
 
 http.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	var q = url.parse(req.url, true).query;
-	var dates = q.year + " " + q.month;
-	res.end(dates);
-}).listen(8080);
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	if(filename == './') {filename = './index'};
+
+	filename = filename + ".html";
+
+	console.log(filename);
+
+	fs.readFile(filename, function(err, data) {
+	if (err) {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		return res.end("404 Not Found");
+		}	
+ 		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	})
+}).listen(PORT);
+
+
+console.log("Server listening on Port 8080...");
